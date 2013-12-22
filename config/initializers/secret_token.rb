04a35d54bb717f8529811cb3e1467942c58631b5
Kumar Kothapalli <P@ssword2012>
2013-12-22 10:49:04 -0700

    Initializing Repository for e2itask

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+E2itask::Application.config.secret_key_base = '048d5e358d52bbd2680a5b6f02fc25a08bb18cca262b7c49a7776f1486dae910bfbb73fb93a0995a74b6fa19272671079879a7d7e2fcc831e6d2008fc205ee85'
