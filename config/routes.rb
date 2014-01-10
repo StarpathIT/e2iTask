c11ecd24172a5fa300f46d5729b464bec25e1ef2
Kumar Kothapalli <P@ssword2012>
2014-01-10 15:54:57 -0700

    UI changes - homepage, about page

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ E2itask::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/task',    to: 'tasks#new',            via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
