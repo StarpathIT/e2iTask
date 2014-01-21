1d3ebae8b07424f9469dc44c38d0dfd16f002038
Kumar Kothapalli <P@ssword2012>
2014-01-20 17:43:13 -0700

    Pricing Plans - homepage, UI changes about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "e2iTask"
+    base_title = "Form Today"
     if page_title.empty?
       base_title
     else
