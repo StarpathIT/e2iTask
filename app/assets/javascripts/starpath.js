841a6990494a15b2cd1101ff9474f8e2c07c74e0
Kumar Kothapalli <P@ssword2012>
2014-03-28 12:43:20 -0600

    css & js minor

--- app/assets/javascripts/starpath.js
+++ app/assets/javascripts/starpath.js
@@ -1,105 +1,77 @@
 //window.onload = init;
-//var animate;
-//var iPhone = null;
-var addtoContainer = null, addOption = null;
-var lb1 = null, lb2 = null, lb3 = null, lb4 = null, opt=null;
+
+var addtoContainer = null;
+var label = null;
 
 function init() {
-//    iPhone = document.getElementById('iPhone');
-//    iPhone.height = 400;
-//    iPhone.width = 250;
-//    iPhone.style.position = 'absolute';
-//    iPhone.style.top = '150px';
-//    iPhone.style.textAlign = 'center';
-//    iPhone.style.left = '20px';
-//    moveRight();
 }
-//
-//function moveRight() {
-//    iPhone.style.left = parseInt(iPhone.style.left) + 30 + 'px';
-//    animate = setTimeout(moveRight, 60); // call moveRight in 20msec
-//    setTimeout(function () {
-//        stop();
-//    }, 2600);
-//}
-//
-//function stop() {
-//    clearTimeout(animate);
-//}
 
 function addTextBox() {
     addtoContainer = document.getElementById('addtoContainer');
-    var error = document.getElementById('error');
+    addtoContainer.hidden = false;
+    var error = document.getElementById('tbError');
     var ip1 = document.getElementById('ip1');
     var errorDiv = document.createElement('div');
     errorDiv.innerHTML = '<div class="center alert alert-danger">* Please Enter Text Box Name </label>';
     error.appendChild(errorDiv);
     if (ip1.value == null || ip1.value == "") {
         error.hidden = false;
+        addtoContainer.hidden = true;
     }
     else {
         error.hidden = true;
-        lb1 = ip1.value;
-        addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label>' + lb1 + '</label><input type="text" id="tb"></div>';
+        label = ip1.value;
+        addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label style="color: #ffffff">' + label + '</label>&nbsp;<input type="text" id="tb"></div>';
         displaySavePanel();
     }
 }
 
 function addComboBox() {
     addtoContainer = document.getElementById('addtoContainer');
-    var error = document.getElementById('error');
-    ip2 = document.getElementById('ip2');
+    addtoContainer.hidden = false;
+    var error = document.getElementById('comboError');
+    var ip2 = document.getElementById('ip2');
     var errorDiv = document.createElement('div');
-    errorDiv.innerHTML = '<div class="center alert alert-danger">* Please Enter Drop Down Name </label>';
+    errorDiv.innerHTML = '<div class="center alert alert-danger">* Missing Drop Down Name or Options</label>';
     error.appendChild(errorDiv);
-    if (ip2.value == null || ip2.value == "") {
+    var opt = document.getElementById('opt');
+    if (ip2.value == null || ip2.value == "" || opt.value == "") {
         error.hidden = false;
+        addtoContainer.hidden = true;
     }
     else {
-        lb2 = ip2.value;
-        opt = document.getElementById('opt');
+        error.hidden = true;
+        label = ip2.value;
+        addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label style="color: #ffffff">' + label + '</label> <select id="tbOption"></select></div>';
         var option = document.createElement("option");
-        if (opt.value == null || opt.value == "") {
-            error.appendChild(errorDiv);
-            errorDiv.innerHTML = '<div class="center alert alert-danger">* Please Enter Options </label>';
-        }
-        else {
-            addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label>' + lb2 + '</label> <select id="tbOption"></select></div>';
-            var tbOption = document.getElementById('tbOption');
-            error.hidden = true;
-            option.text = opt.value;
-            tbOption.add(option);
-            addDDOption();
-        }
-    }
-    displaySavePanel();
-}
-
-function addDDOption() {
-addOption = document.getElementById('dropDownOptions');
-opt.value = "";
+        option.text = opt.value;
+        var tbOption = document.getElementById('tbOption');
+        tbOption.add(option);
+        displaySavePanel();}
 }
 
 function addUploadField() {
     addtoContainer = document.getElementById('addtoContainer');
-    var error = document.getElementById('error');
+    addtoContainer.hidden = false;
+    var error = document.getElementById('uploadError');
     var ip3 = document.getElementById('ip3');
     var errorDiv = document.createElement('div');
     error.appendChild(errorDiv);
     errorDiv.innerHTML = '<div class="center alert alert-danger">* Please Enter Upload Field Name </label>';
     if (ip3.value == null || ip3.value == "") {
         error.hidden = false;
+        addtoContainer.hidden = true;
     }
     else {
         error.hidden = true;
         lb3 = ip3.value;
-        addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label>' + lb3 + '</label><input type="file" id="tb"></div>';
+        addtoContainer.innerHTML += '<div class="col-lg-offset-0"><label style="color: #ffffff">' + lb3 + '</label><input class="center" type="file" id="tb" value=""></div>';
         displaySavePanel();
     }
 }
 
 function addPaymentOption() {
-    var error = document.getElementById('error');
+    var error = document.getElementById('comboError');
     var errorDiv = document.createElement('div');
     error.appendChild(errorDiv);
     errorDiv.innerHTML = '<div class="center alert alert-danger">* PAYMENT WIDGET UNDER CONSTRUCTION</label>';
@@ -108,6 +80,7 @@ function addPaymentOption() {
 
 function addDateField() {
     addtoContainer = document.getElementById('addtoContainer');
+    addtoContainer.hidden = false;
     var error = document.getElementById('error');
     var ip5 = document.getElementById('ip5');
     var errorDiv = document.createElement('div');
@@ -115,11 +88,12 @@ function addDateField() {
     errorDiv.innerHTML = '<div class="center alert alert-danger">* Please Enter Date Field Name </label>';
     if (ip5.value == null || ip5.value == "") {
         error.hidden = false;
+        addtoContainer.hidden = true;
     }
     else {
         error.hidden = true;
         lb5 = ip5.value;
-        addtoContainer.innerHTML += '<p class="tb"><label>' + lb5 + '</label>  <input type="text" id="tb"></p><br>';
+        addtoContainer.innerHTML += '<p class="tb"><label style="color: #ffffff">' + lb5 + '</label>  <input type="text" id="tb"></p><br>';
         displaySavePanel();
     }
 }
